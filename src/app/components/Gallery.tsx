6f854bd258278b724d2dcffe3ed9e60cebfa5a08
gitdagray <DaveOnEleven@gmail.com>
2023-07-28 06:24:28 -0500

    Completed lesson 7

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -1,6 +1,7 @@
 import fetchImages from "@/lib/fetchImages"
 import type { ImagesResults } from "@/models/Images"
 import ImgContainer from "./ImgContainer"
+import addBlurredDataUrls from "@/lib/getBase64"
 
 export default async function Gallery() {
 
@@ -10,11 +11,13 @@ export default async function Gallery() {
 
     if (!images) return <h2 className="m-4 text-2xl font-bold">No Images Found</h2>
 
+    const photosWithBlur = await addBlurredDataUrls(images)
+
     return (
         <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
 
-            {images.photos.map(photo => (
-                <ImgContainer photo={photo} />
+            {photosWithBlur.map(photo => (
+                <ImgContainer key={photo.id} photo={photo} />
             ))}
 
         </section>

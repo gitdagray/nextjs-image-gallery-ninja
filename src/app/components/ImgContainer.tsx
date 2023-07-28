95e61af0c848933864d4540e845a481e57057956
gitdagray <DaveOnEleven@gmail.com>
2023-07-27 19:02:21 -0500

    Completed lesson 6

--- src/app/components/ImgContainer.tsx
+++ src/app/components/ImgContainer.tsx
@@ -7,12 +7,13 @@ type Props = {
 
 export default function ImgContainer({ photo }: Props) {
     return (
-        <div key={photo.id} className="h-64 bg-gray-200 rounded-xl">
+        <div key={photo.id} className="h-64 bg-gray-200 rounded-xl relative overflow-hidden group">
             <Image
                 src={photo.src.large}
                 alt={photo.alt}
-                width={250}
-                height={250}
+                fill={true}
+                sizes="(min-width: 1280px) 278px, (min-width: 1040px) calc(12.73vw + 118px), (min-width: 800px) 33.18vw, (min-width: 540px) 50vw, calc(100vw - 16px)"
+                className="object-cover group-hover:opacity-75"
             />
         </div>
     )
